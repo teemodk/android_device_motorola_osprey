Multirom-Lux/android_device_motorola_lux

Fix some things
this fixes injecting boot, touch and building

vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo omni_lux-userdebug
+add_lunch_combo omni_lux-eng

teemodk/android_device_motorola_osprey

Conflicts:
	vendorsetup.sh

vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo omni_osprey-userdebug
+add_lunch_combo omni_osprey-eng